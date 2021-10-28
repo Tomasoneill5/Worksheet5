a94e43b26e3af5d1aebac71744ea1cd8993c9557
Tomasoneill5 <92168523+Tomasoneill5@users.noreply.github.com>
2021-10-28 23:41:50 +0100

    Add files via upload

--- CardNumWrk5.py
+++ CardNumWrk5.py
@@ -1,17 +1,15 @@
 cardNum=input('please enter your card number: ')
 cardNumLen=len(cardNum)
-totalStep2=0
 total=0
 number=int(cardNum)
 
 if (cardNumLen<13) or (cardNumLen>16):
-    print('this card is invalid, must be between 13 and 16 digits')
+    print(cardNum, 'is invalid')
 else:
     position=1
     while number>0:
         digit= number % 10
         number= number//10
-        print('******  Number is now :   ',number)
         if position % 2==0:
             digit= digit * 2
         
@@ -27,20 +25,17 @@ else:
         if (number<100) and (number>=10):
             secondDigit= number % 10
    
-            
-print('first digit: ',firstDigit)
-print('second digit: ',secondDigit)
 if (firstDigit<3) or (firstDigit>6):
-    print(cardNum, 'is invalid 1')
+    print(cardNum, 'is invalid')
     
 elif (firstDigit==3) and (secondDigit!=7):
-    print(cardNum, 'is invalid 2')
+    print(cardNum, 'is invalid')
     
 elif  total%10!=0:
-    print(cardNum, 'is invalid 3')
+    print(cardNum, 'is invalid')
               
 else:
-    print(cardNum, 'is valid 4')
+    print(cardNum, 'is valid')
             
         
         
